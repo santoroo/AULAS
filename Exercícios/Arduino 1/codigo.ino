1fce30c480f739df0f15fb289bce054685caeda1
Gabriel Santoro <79268036+santoroo@users.noreply.github.com>
2022-03-21 12:39:40 -0300

    Update codigo.ino

--- Exercícios/Arduino 1/codigo.ino
+++ Exercícios/Arduino 1/codigo.ino
@@ -1,1 +1,14 @@
-sasa
+// C++ code
+//
+void setup()
+{
+  pinMode(3, OUTPUT);
+}
+
+void loop()
+{
+       digitalWrite (3, HIGH);
+       delay (500);
+       digitalWrite (3, LOW); 
+       delay (500);
+}
