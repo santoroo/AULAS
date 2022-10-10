855b54daaed081f81c721c0703bca04b903760c3
Gabriel Santoro <79268036+santoroo@users.noreply.github.com>
2022-10-10 16:21:40 -0300

    Create codigo.ino

---
+++ 3 Trimestre/Aula dia 10 do 10/codigo.ino
@@ -0,0 +1,23 @@
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+}
+
+void loop()
+{
+long tecladoA5=0;
+tecladoA5 =analogRead(A5);
+  if(tecladoA5 == 205){
+       digitalWrite(2, HIGH);
+  }else{
+   digitalWrite(2, LOW); 
+  }
+  
+Serial.println(tecladoA5);
+delay(100);
+}
